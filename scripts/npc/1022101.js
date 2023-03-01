cad1f35e97bc78e9216fd24d561c2b03dd32db36
Mihaelov <slava.m@pandats.com>
2023-03-01 23:40:02 +0200

    npc tests

--- scripts/npc/1022101.js
+++ scripts/npc/1022101.js
@@ -23,33 +23,7 @@ Jan Christian Meyer <vimes@odinms.de>
  Rooney - Happyville Warp NPC
  **/
 
-var status;
-
 function start() {
-    status = -1;
-    action(1, 0, 0);
-}
-
-function action(mode, type, selection) {
-    if (mode == -1) {
-        cm.dispose();
-    } else {
-        if (mode == 0 && type > 0) {
-            cm.dispose();
-            return;
-        }
-        if (mode == 1) {
-            status++;
-        } else {
-            status--;
-        }
-
-        if (status == 0) {
-            cm.sendYesNo("Santa told me to go to here, only he didn't told me when...  I hope I'm here on the right time! Oh! By the way, I'm Rooney, I can take you to #bHappyVille#k. Are you ready to go?");
-        } else {
-            cm.getPlayer().saveLocation("HAPPYVILLE");
-            cm.warp(209000000, 0);
-            cm.dispose();
-        }
-    }
+    cm.sendOk("I am an NPC without a status.");
+    cm.dispose();
 }
